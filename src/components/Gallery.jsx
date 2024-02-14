3b1633ed050f8900deab3be95e29ad52f752af6b
Dipankar Prasad <iamdpunkr@gmail.com>
2024-02-14 12:47:18 +0530

    feat: add image zoom functionality

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,4 +1,5 @@
-import Masonry from "react-masonry-css"
+import Masonry from "react-masonry-css";
+import { useState } from "react";
 const breakpointColumnsObj = {
   default: 4,
   1100: 3,
@@ -6,84 +7,75 @@ const breakpointColumnsObj = {
   500: 1
 };
 
+const imageSrc = [
+  "https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(70).webp",
+  "https://images.pexels.com/photos/2147029/pexels-photo-2147029.jpeg?cs=srgb&dl=pexels-laura-stanley-2147029.jpg&fm=jpg",
+  "https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(72).webp",
+  "https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp",
+  "https://i.pinimg.com/236x/b3/81/6e/b3816ef52daa13277134887e0cc7f314.jpg",
+  "https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(70).webp",
+  "https://images.pexels.com/photos/2147029/pexels-photo-2147029.jpeg?cs=srgb&dl=pexels-laura-stanley-2147029.jpg&fm=jpg",
+  "https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(72).webp",
+  "https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp",
+  "https://i.pinimg.com/236x/b3/81/6e/b3816ef52daa13277134887e0cc7f314.jpg",
+  "https://images.pexels.com/photos/2147029/pexels-photo-2147029.jpeg?cs=srgb&dl=pexels-laura-stanley-2147029.jpg&fm=jpg",
+  "https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(70).webp",
+  
+  "https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(72).webp",
+  "https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp",
+  "https://i.pinimg.com/236x/b3/81/6e/b3816ef52daa13277134887e0cc7f314.jpg",
+  "https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(70).webp",
+  
+  "https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(72).webp",
+  "https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp",
+  "https://images.pexels.com/photos/2147029/pexels-photo-2147029.jpeg?cs=srgb&dl=pexels-laura-stanley-2147029.jpg&fm=jpg",
+  "https://i.pinimg.com/236x/b3/81/6e/b3816ef52daa13277134887e0cc7f314.jpg",
+
+]
 const Gallery = () => {
+
+  const [modal, setModal] = useState(false);
+  const [selectedImg, setImg] = useState("");
+
+
   return (
-        
+    <>
     <Masonry
     breakpointCols={breakpointColumnsObj}
     className="my-masonry-grid"
     columnClassName="my-masonry-grid_column">
-    {/* array of JSX items */}
+    {
+      imageSrc.map((src, i)=>{
+      return  (
+            <div key={i} className="w-full " onClick={()=>{ setImg(src); setModal(true);}}>
+                <img
+                  alt="gallery"
+                  className="block  w-full rounded-lg object-cover object-center hover:scale-105 transition ease-in-out duration-1000"
+                  src={src} />
+              </div>
+        )
+      })
+    }
   
+                 
         
+    </Masonry>
 
-              <div className="w-full ">
-                <img
-                  alt="gallery"
-                  className="block  w-full rounded-lg object-cover object-center hover:scale-105 transition ease-in-out duration-1000"
-                  src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(70).webp" />
-              </div>
-              <div className="w-full  bg-white">
-                <img
-                  alt="gallery"
-                  className="block  w-full rounded-lg object-cover object-center hover:scale-105 transition ease-in-out duration-1000"
-                  src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(72).webp" />
-              </div>
-              <div className="w-full  ">
-                <img
-                  alt="gallery"
-                  className="block  w-full rounded-lg object-cover object-center hover:scale-105 transition ease-in-out duration-1000"
-                  src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp" />
-              </div>
-            
-              <div className="w-full  ">
-                <img
-                  alt="gallery"
-                  className="block  w-full rounded-lg object-cover object-center hover:scale-105 transition ease-in-out duration-1000"
-                  src="https://i.pinimg.com/236x/b3/81/6e/b3816ef52daa13277134887e0cc7f314.jpg" />
-              </div>
-
-              <div className="w-full ">
-                <img
-                  alt="gallery"
-                  className="block w-full rounded-lg object-cover object-center hover:scale-105 transition ease-in-out duration-1000"
-                  src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(74).webp" />
-              </div>
-              <div className="w-full  ">
-                <img
-                  alt="gallery"
-                  className="block w-full rounded-lg object-cover object-center hover:scale-105 transition ease-in-out duration-1000"
-                  src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(75).webp" />
-              </div>
-           
-              <div className="w-full  ">
-                <img
-                  alt="gallery"
-                  className="block  w-full rounded-lg object-cover object-center hover:scale-105 transition ease-in-out duration-1000"
-                  src="https://i.pinimg.com/236x/b3/81/6e/b3816ef52daa13277134887e0cc7f314.jpg" />
-              </div>
-              <div className="w-full ">
-                <img
-                  alt="gallery"
-                  className="block w-full rounded-lg object-cover object-center hover:scale-105 transition ease-in-out duration-1000"
-                  src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(74).webp" />
-              </div>
-              <div className="w-full  ">
-                <img
-                  alt="gallery"
-                  className="block w-full rounded-lg object-cover object-center hover:scale-105 transition ease-in-out duration-1000"
-                  src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(75).webp" />
-              </div>
-            
-              <div className="w-full  ">
-                <img
-                  alt="gallery"
-                  className="block  w-full rounded-lg object-cover object-center hover:scale-105 transition ease-in-out duration-1000"
-                  src="https://i.pinimg.com/236x/b3/81/6e/b3816ef52daa13277134887e0cc7f314.jpg" />
-              </div>
-            
-        
-        </Masonry>
+     {modal && (
+        <div className="fixed top-0 left-0 w-full h-screen overflow-hidden bg-black bg-opacity-80 flex justify-center items-center z-50">
+          <div className=" max-w-4xl relative">
+            <img src={selectedImg} className="w-full max-h-screen"/>
+           <button
+              onClick={() =>{ setModal(false); setImg("")}}
+              className="absolute right-0 top-0 md:-right-8 lg:-right-12 bg-red-500 text-white px-4 py-2 rounded-lg z-50"
+            >
+              X
+            </button>
+          </div>
+        </div>
+      )}
+      </>
+    
   )
 }
 
